3e7a9f4af97d5ec6f5beae68ac715fa1d042146e
Tran Chi Hieu <hieu57130154@gmail.com>
2020-11-11 21:24:51 +0700

    first commit

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String TrangChu() {
         return "Xin chào việt nam! tôi là Trần Chí Hiếu";
     }
